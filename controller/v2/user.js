df0702881ad1f93ad01d151fe62e6d5697bc296b
xinshihao <785296341@qq.com>
2023-05-19 17:09:03 +0800

    feat: 增加根据id获取用户信息接口

--- controller/v2/user.js
+++ controller/v2/user.js
@@ -3,7 +3,8 @@ const AddressComponent = require("../../prototype/addressComponent");
 const UserModal = require("../../models/v2/user");
 const UserInfoModal = require("../../models/v2/userInfo");
 const formidable = require("formidable");
-const dayjs = require('dayjs');
+const dayjs = require("dayjs");
+
 
 class User extends AddressComponent {
   constructor() {
@@ -25,13 +26,13 @@ class User extends AddressComponent {
         }
         const user = await UserModal.findOne({ username });
         const user_id = await this.getId("user_id");
-      
-        console.log('user',user);
+
+        console.log("user", user);
 
         if (!user) {
           const cityInfo = await this.guessPosition(req); // 存入周边城市信息
           const newUser = { user_id, is: user_id, username, password };
-          const registe_time = dayjs().format('YYYY-MM-DD HH:mm:ss');
+          const registe_time = dayjs().format("YYYY-MM-DD HH:mm:ss");
           const newUserInfo = {
             username,
             user_id,
@@ -41,10 +42,10 @@ class User extends AddressComponent {
           };
           await UserModal.create(newUser);
           const user_info = await UserInfoModal.create(newUserInfo);
-          console.log('userInfo',user_info);
+          console.log("userInfo", user_info);
           req.session.user_id = user_id;
-       
-          res.send({user_info});
+
+          res.send(user_info);
         } else if (String(password) !== String(user.password)) {
           // 判断密码是否正确
           console.log("用户登录密码错误");
@@ -58,10 +59,7 @@ class User extends AddressComponent {
           const userinfo = await UserInfoModal.findOne({
             user_id: user.user_id,
           });
-          res.send({
-            userinfo,
-            code:0
-          });
+          res.send(userinfo);
         }
       });
     } catch (err) {
@@ -70,6 +68,26 @@ class User extends AddressComponent {
         status: -1,
         err,
       });
+    }
+  }
+
+  async getUserInfoById(req, res, next) {
+    const user_id = req.query.user_id;
+    if (!user_id || !Number(user_id)) {
+      res.send({
+        stauts: -1,
+        message: "参数错误，未获取到用户Id",
+      });
+    }
+    try {
+      const userInfo = await UserInfoModal.findOne({ user_id });
+      console.log('userInfo',userInfo);
+      res.send(userInfo);
+    } catch (err) {
+      res.send({
+        message:"通过ID获取用户信息失败",
+        status:-1
+      })
     }
   }
 }
--- routes/v2.js
+++ routes/v2.js
@@ -5,7 +5,7 @@ const UserHandle = require("../controller/v2/user");
 const router = express.Router();
 
 router.post('/login',UserHandle.login);
-
+router.get('/getUser',UserHandle.getUserInfoById);
 
 
 module.exports = router;
